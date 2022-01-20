0e59429f9f446df6dfaf0dd2668295e5ae211010
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-01-20 22:30:56 +0200

    Updated: 2022-01-20 22:30:56

--- config/extra.nix
+++ config/extra.nix
@@ -17,6 +17,9 @@ in {
     #   };
     #   inactiveInterval = 5;
     # };
+    poweralertd = {
+      enable = true;
+    };
     xidlehook = {
       enable = true;
       not-when-fullscreen = true;
@@ -38,43 +41,43 @@ in {
     };
     flameshot = { enable = true; };
   };
-  systemd.user.timers."lowbatt" = {
-    Unit = {
-      Description = "check battery level";
-    };
-    Timer ={
-      OnBootSec = "1m";
-      OnUnitInactiveSec = "1m";
-    };
-    Install.WantedBy = [ "timers.target" ];
-  };
+  # systemd.user.timers."lowbatt" = {
+  #   Unit = {
+  #     Description = "check battery level";
+  #   };
+  #   Timer ={
+  #     OnBootSec = "1m";
+  #     OnUnitInactiveSec = "1m";
+  #   };
+  #   Install.WantedBy = [ "timers.target" ];
+  # };
 
-  systemd.user.services."lowbatt" = {
-    Unit = {
-      Description = "battery level notifier";
-    };
-    Service = {
-      PassEnvironment = "DISPLAY";
-      ExecStart = toString (pkgs.writeScript "lowbatt.sh" ''
-      #! ${pkgs.runtimeShell} -el
-      export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/capacity)
-      export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/status)
-      if [[ $battery_capacity -le ${
-        builtins.toString notifyCapacity
-      } && $battery_status = "Discharging" ]]; then
-          ${pkgs.dunst}/bin/dunstify --urgency=critical --icon=battery_empty "Battery Low" "You should probably plug-in."
-      fi
-      if [[ $battery_capacity -le ${
-        builtins.toString suspendCapacity
-      } && $battery_status = "Discharging" ]]; then
-          ${pkgs.dunst}/bin/dunstify --urgency=critical -icon=battery_empty "Battery Critically Low" "Computer will suspend in 60 seconds."
-          sleep 60s
-          battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/status)
-          if [[ $battery_status = "Discharging" ]]; then
-              systemctl suspend
-          fi
-      fi
-    '');
-    };
-  };
+  # systemd.user.services."lowbatt" = {
+  #   Unit = {
+  #     Description = "battery level notifier";
+  #   };
+  #   Service = {
+  #     PassEnvironment = "DISPLAY";
+  #     ExecStart = toString (pkgs.writeScript "lowbatt.sh" ''
+  #     #! ${pkgs.runtimeShell} -el
+  #     export battery_capacity=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/capacity)
+  #     export battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/status)
+  #     if [[ $battery_capacity -le ${
+  #       builtins.toString notifyCapacity
+  #     } && $battery_status = "Discharging" ]]; then
+  #         ${pkgs.dunst}/bin/dunstify --urgency=critical --icon=battery_empty "Battery Low" "You should probably plug-in."
+  #     fi
+  #     if [[ $battery_capacity -le ${
+  #       builtins.toString suspendCapacity
+  #     } && $battery_status = "Discharging" ]]; then
+  #         ${pkgs.dunst}/bin/dunstify --urgency=critical -icon=battery_empty "Battery Critically Low" "Computer will suspend in 60 seconds."
+  #         sleep 60s
+  #         battery_status=$(${pkgs.coreutils}/bin/cat /sys/class/power_supply/${battery}/status)
+  #         if [[ $battery_status = "Discharging" ]]; then
+  #             systemctl suspend
+  #         fi
+  #     fi
+  #   '');
+  #   };
+  # };
 }
