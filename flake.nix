7b2503f62f82ef47416bf1a8eb920c4ba41b508c
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-06-23 19:37:15 +0300

    upgrade: bump to 24.05

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
     # Official NixOS package source, using nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     # home-manager, used for managing user configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
