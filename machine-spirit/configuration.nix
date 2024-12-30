ff9bba59a3d1c93876084a655f2a8eb73ff9e026
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2024-12-30 23:29:31 +0200

    fix: after lspci and google

--- machine-spirit/configuration.nix
+++ machine-spirit/configuration.nix
@@ -16,7 +16,7 @@
 
   networking.hostName = "machine-spirit"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   networking = {
@@ -25,6 +25,7 @@
       insertNameservers = [ "127.0.0.1" "::1" ];
       enable = true;
       dns = "none";
+      wifi.backend = "iwd";
     };
   };
 
--- machine-spirit/hardware-configuration.nix
+++ machine-spirit/hardware-configuration.nix
@@ -8,19 +8,38 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "nvme" "xhci_pci" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-intel" "wl" "coretemp" "applesmc"];
-  boot.kernelParams = ["radeon.si_support=0" "amdgpu.si_support=1"];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+
+  boot = {
+    initrd.kernelModules = [
+      "applesmc"
+      "applespi"
+      "intel_lpss_pci"
+      "spi_pxa2xx_platform"
+      "kvm-intel"
+      "dm-snapshot"
+    ];
+    blacklistedKernelModules = [
+      "b43"
+      "ssb"
+      "brcmfmac"
+      "brcmsmac"
+      "bcma"
+    ];
+    extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+    kernelParams = ["radeon.si_support=0" "amdgpu.si_support=1"];
+  };
+
+  # boot.initrd.availableKernelModules = [ "ehci_pci" "nvme" "xhci_pci" ];
+  # boot.initrd.kernelModules = [ "dm-snapshot"];
+  # boot.kernelModules = [ "kvm-intel" "coretemp" "applesmc" "wl"];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
   # blacklist similar modules to avoid collision, this might save my wifi
-  boot.blacklistedKernelModules = [ "b43" "bcma" ];
+  # boot.blacklistedKernelModules = [ "b43" "bcma" ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp11s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp12s0.useDHCP = lib.mkDefault true;
 
