7fafe92aa056ecf36c7ac5f2bc9ce93a8ee65864
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2025-02-12 00:19:58 +0200

    fix: add nbetter model

--- machine-spirit/services.nix
+++ machine-spirit/services.nix
@@ -2,10 +2,23 @@
 {
 
   services.ollama  = {
-    enabled = true;
+    enable = true;
     acceleration = false;
+    loadModels = ["qwen2.5-coder:1.5b"];
+  };
+
+  services.open-webui = {
+    enable = true;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
+    port = 8084;
+    host = "192.168.0.109";
     openFirewall = true;
-    loadModels = ["codeqwen"];
   };
   services.nomad = {
     enable = true;
