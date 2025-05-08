7bc63be7690832bb6ec975fcbf5fce47a1ac883a
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2025-05-08 18:30:59 +0300

    fix: remove this, need to reorganize everything

--- three-of-five/hardware-configuration.nix
+++ three-of-five/hardware-configuration.nix
@@ -24,19 +24,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  boot.kernelParams = [
-    "i915.enable_guc=2"
-  ];
-
-  # VP9 decoding not supported when using intel-media-driver
-  # https://github.com/intel/media-driver/issues/1024
-  # NixOS Wiki recommends using the legacy intel-vaapi-driver with the hybrid codec over that one for Skylake.
-  # https://wiki.nixos.org/wiki/Accelerated_Video_Playback
-  hardware.intelgpu = {
-    vaapiDriver = "intel-vaapi-driver";
-    enableHybridCodec = true;
-  };
-
   swapDevices = [ {
     device = "/var/lib/swapfile";
     size = 16*1024;
