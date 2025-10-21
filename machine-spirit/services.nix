b923d496b3179f27ae236cd6bbf3e8ebf4dbdba2
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2025-10-21 23:21:32 +0300

    fix: no webauth for openui

--- machine-spirit/services.nix
+++ machine-spirit/services.nix
@@ -22,6 +22,7 @@
       SCARF_NO_ANALYTICS = "True";
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+      WEBUI_AUTH="False";
     };
     port = 8084;
     host = "0.0.0.0";
