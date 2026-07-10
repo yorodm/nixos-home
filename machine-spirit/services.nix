438a2a71bd6f966b7bff82d649d57b191f061a55
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2026-07-10 09:09:32 +0300

    update: some stuff

--- machine-spirit/services.nix
+++ machine-spirit/services.nix
@@ -7,4 +7,20 @@
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
+  services.thermald.enable = true;
+
+  systemd.services.disable-turbo = {
+    description = "Disable CPU Turbo Boost";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      if [ -w /sys/devices/system/cpu/intel_pstate/no_turbo ]; then
+        echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+      elif [ -w /sys/devices/system/cpu/cpufreq/boost ]; then
+        echo 0 > /sys/devices/system/cpu/cpufreq/boost
+      fi
+    '';
+  };
 }
